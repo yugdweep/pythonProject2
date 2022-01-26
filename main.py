f239d3546c8e07aa2d4f3ec41fc6d9bf608ce9ef
Yugdweep <yugdweepkumarverma@gmail.com>
2022-01-26 15:34:19 +0530

    changes

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-url = 'https://www.ndtv.com/india#pfrom=home-ndtv_mainnavgation'
-page = requests.get(url)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-lists = soup.find_all('div', class_="news_Itm-cont")
-
-with open('ndtvnews.csv', 'w', newline='') as f:
+with open('ntdvnewsyug.csv', 'w', newline='') as f:
     csv_writer = writer(f)
     header = ['Title', 'Date', 'Summary']
     csv_writer.writerow(header)
 
-    for list in lists:
-        title = list.find('h2', class_="newsHdng").text
-        date = list.find('span', class_="posted-by").text
-        topic = list.find('p', class_="newsCont").text
+    for i in range(1,11):
+        url = "https://www.ndtv.com/india/page-"+str(i)
 
-        info = [title, date, topic]
-        csv_writer.writerow(info)
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        lists = soup.find_all('div', class_="news_Itm-cont")
+
+        for list in lists:
+            title = list.find('h2', class_="newsHdng").text.strip()
+            date = list.find('span', class_="posted-by").text.strip()
+            topic = list.find('p', class_="newsCont").text.strip()
+
+            info = [title, date, topic]
+            csv_writer.writerow(info)
+            print(info)
+
+
+
 
 
 
